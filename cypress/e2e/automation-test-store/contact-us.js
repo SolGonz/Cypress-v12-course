7fb7d91e28f0d107d9ae991ef04605c8c7ccf7ab
Solange Gonzalez <solgonzalezaraya@gmail.com>
2023-02-26 18:22:33 -0300

    Test contact us form via Automation Test Store from selectors lectures

---
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -0,0 +1,13 @@
+describe('Test contact us form via Automation Test Store', () => { 
+    
+    it('Should be able to submit a successful submission via contact us form as all fields are required', () => {
+        cy.visit("https://automationteststore.com/");
+        cy.get('a[href="https://automationteststore.com/index.php?rt=content/contact"]').click();
+        cy.get('#ContactUsFrm_first_name').type("Solange");
+        cy.get('#ContactUsFrm_email').type("sol@test.com");
+        cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?");
+        cy.get('button[title="Submit"]').click();
+
+    });
+
+ })
